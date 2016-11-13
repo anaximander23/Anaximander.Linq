89c8af35c7101d34813b03744dd422f7235c5e57
Nicholas Budd <nickbudd@gmail.com>
2016-11-13 13:13:08 +0000

    Fixed namespace for Box methods

--- src/Anaximander.Linq.Shared/Box/IEnumerableExtensions.Box.cs
+++ src/Anaximander.Linq.Shared/Box/IEnumerableExtensions.Box.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Anaximander.Linq.Shared.Box
+namespace Anaximander.Linq
 {
     public static partial class IEnumerableExtensions
     {
