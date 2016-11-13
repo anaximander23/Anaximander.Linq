d1a34697f4146cee5a2327cf7c921de5a0963871
Nicholas Budd <nickbudd@gmail.com>
2016-11-13 12:53:16 +0000

    Added metadata for packaging.

--- src/Anaximander.Linq/Properties/AssemblyInfo.cs
+++ src/Anaximander.Linq/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Anaximander.Linq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Useful LINQ extension methods and utility classes for digging deeper into data.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("anaximander")]
 [assembly: AssemblyProduct("Anaximander.Linq")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
