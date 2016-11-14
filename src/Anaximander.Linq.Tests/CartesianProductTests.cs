43f2820c6a989ba7ccd71393d946c7b33df32e0c
anaximander <nickbudd@gmail.com>
2016-11-14 14:47:37 +0000

    Test stubs.

---
+++ src/Anaximander.Linq.Tests/CartesianProductTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace Anaximander.Linq.Tests
+{
+    public class CartesianProductTests
+    {
+        [Fact(Skip ="Not implemented")]
+        public void GivenEmptyOuterCollection_ThrowsInvalidOperationException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact(Skip = "Not implemented")]
+        public void GivenSingleCollection_ReturnsOriginalCollectionAsIndividuals()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory(Skip = "Not implemented")]
+        [InlineData]
+        public void GivenTwoCollections_ReturnsCorrectCombinations()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Theory(Skip = "Not implemented")]
+        [InlineData]
+        public void GivenNCollections_ReturnsCorrectCombinations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
