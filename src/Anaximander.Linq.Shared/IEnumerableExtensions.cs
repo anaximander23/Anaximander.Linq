8c442e9379d49a4c8577932c58cd814fa05e8a14
anaximander <nickbudd@gmail.com>
2016-11-29 11:43:00 +0000

    OrderToMatch tests and fixes.

--- src/Anaximander.Linq.Shared/IEnumerableExtensions.cs
+++ src/Anaximander.Linq.Shared/IEnumerableExtensions.cs
@@ -6,10 +6,32 @@ namespace Anaximander.Linq
 {
     public static partial class IEnumerableExtensions
     {
-        public static IOrderedEnumerable<T> OrderToMatch<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector, IEnumerable<TKey> ordering)
+        /// <summary>
+        /// Orders a collection using a provided collection to set the order. Items whose sort key does not appear in the provided order appear last, in their original order.
+        /// </summary>
+        /// <param name="source">A collection of items to be sorted.</param>
+        /// <param name="sortKeySelector">A func to derive a key on which to sort from the items in the source collection.</param>
+        /// <param name="ordering">A collection denoting the desired order</param>
+        /// <returns>A collection sorted to match the ordering collection, with missing items sorted to the end.</returns>
+        public static IOrderedEnumerable<T> OrderToMatch<T, TKey>(this IEnumerable<T> source, Func<T, TKey> sortKeySelector, IEnumerable<TKey> ordering)
         {
             TKey[] orderArray = ordering.ToArray();
-            return source.OrderBy<T, int>(x => Array.IndexOf(orderArray, selector(x)));
+
+            if (!orderArray.Any())
+            {
+                throw new ArgumentException("Ordering collection cannot be empty.", nameof(ordering));
+            }
+
+            T[] sourceArray = source.ToArray();
+
+            return sourceArray
+                .OrderBy(x =>
+                    {
+                        int index = Array.IndexOf(orderArray, sortKeySelector(x));
+
+                        return (index >= 0 ? index : Int32.MaxValue);
+                    })
+                .ThenBy(x => Array.IndexOf(sourceArray, x));
         }
 
         public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> func)
---
+++ src/Anaximander.Linq.Tests.Shared/OrderToMatchTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Anaximander.Linq.Tests
+{
+    public class OrderToMatchTests
+    {
+        [Fact]
+        public void GivenNullCollection_ThrowsArgumentNullException()
+        {
+            IEnumerable<int> collection = null;
+            var order = new[] { 1, 2, 3 };
+
+            Assert.Throws<ArgumentNullException>(() => collection.OrderToMatch(x => x, order).ToList());
+        }
+
+        [Fact]
+        public void GivenNullOrderingCollection_ThrowsArgumentNullException()
+        {
+            IEnumerable<int> collection = new[] { 1, 2, 3 };
+            IEnumerable<int> order = null;
+
+            Assert.Throws<ArgumentNullException>(() => collection.OrderToMatch(x => x, order).ToList());
+        }
+
+        [Fact]
+        public void GivenEmptyCollection_ReturnsEmptyCollection()
+        {
+            IEnumerable<int> collection = new List<int>();
+
+            List<IEnumerable<int>> result = collection.Permute().ToList();
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GivenEmptyOrderingCollection_ThrowsArgumentException()
+        {
+            IEnumerable<int> collection = new[] { 1, 2, 3 };
+            IEnumerable<int> order = new List<int>();
+
+            Assert.Throws<ArgumentException>(() => collection.OrderToMatch(x => x, order).ToList());
+        }
+
+        [Fact]
+        public void GivenValidCollection_ReturnsCorrectOrder()
+        {
+            var collection = new Dictionary<int, string>
+            {
+                [3] = "c",
+                [2] = "a",
+                [1] = "b"
+            };
+
+            var order = new[] { 2, 3, 1 };
+
+            var expected = new Dictionary<int, string>
+            {
+                [2] = "a",
+                [3] = "c",
+                [1] = "b"
+            }
+            .ToArray();
+
+            var result = collection.OrderToMatch(x => x.Key, order).ToArray();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GivenMissingOrderKey_ReturnsCorrectOrder()
+        {
+            var collection = new Dictionary<int, string>
+            {
+                [3] = "c",
+                [2] = "a",
+                [1] = "b"
+            };
+
+            var order = new[] { 2, 3 };
+
+            var expected = new Dictionary<int, string>
+            {
+                [2] = "a",
+                [3] = "c",
+                [1] = "b"
+            }
+            .ToArray();
+
+            var result = collection.OrderToMatch(x => x.Key, order).ToArray();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GivenMultipleMissingOrderKeys_ReturnsCorrectOrder()
+        {
+            var collection = new Dictionary<int, string>
+            {
+                [5] = "d",
+                [3] = "c",
+                [2] = "a",
+                [1] = "b"
+            };
+
+            var order = new[] { 2, 3 };
+
+            var expected = new Dictionary<int, string>
+            {
+                [2] = "a",
+                [3] = "c",
+                [5] = "d",
+                [1] = "b"
+            }
+            .ToArray();
+
+            var result = collection.OrderToMatch(x => x.Key, order).ToArray();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
