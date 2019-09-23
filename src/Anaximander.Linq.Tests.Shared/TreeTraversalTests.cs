366bd29ac570432fc75b44de09a963aa4132fceb
anaximander <nickbudd@gmail.com>
2019-09-23 14:31:42 +0100

    Added the beginning of some helpers for working with treelike data structures.

---
+++ src/Anaximander.Linq.Tests.Shared/TreeTraversalTests.cs
@@ -0,0 +1,130 @@
+﻿using Anaximander.Linq.TreeTraversal;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Anaximander.Linq.Tests
+{
+    public class TreeTraversalTests
+    {
+        [Fact]
+        public void WhenTraversingSimpleTree_FindsCorrectPaths()
+        {
+            IEnumerable<NTreeNode> tree = GenerateTree();
+
+            IEnumerable<IEnumerable<string>> paths = TreeTraverser
+                .GetTraversalPaths(tree.First(), node => node.Children)
+                .Select(path => path.Select(node => node.Label).ToArray())
+                .ToArray();
+
+            string[][] expected = new[]
+            {
+                new[] { "A", "B", "D", "H" },
+                new[] { "A", "B", "D", "I" },
+                new[] { "A", "B", "E", "J" },
+                new[] { "A", "B", "E", "K" },
+                new[] { "A", "C", "F", "L" },
+                new[] { "A", "C", "F", "M" },
+                new[] { "A", "C", "G", "N" },
+                new[] { "A", "C", "G", "O" },
+            };
+
+            Assert.Equal(expected, paths);
+        }
+
+        private IEnumerable<NTreeNode> GenerateTree()
+        {
+            return new[]
+            {
+                new NTreeNode
+                {
+                    Label = "A",
+                    Children = new[]
+                    {
+                        new NTreeNode
+                        {
+                            Label = "B",
+                            Children = new[]
+                            {
+                                new NTreeNode
+                                {
+                                    Label = "D",
+                                    Children = new[]
+                                    {
+                                        new NTreeNode
+                                        {
+                                            Label = "H"
+                                        },
+                                        new NTreeNode
+                                        {
+                                            Label = "I"
+                                        }
+                                    }
+                                },
+                                new NTreeNode
+                                {
+                                    Label = "E",
+                                    Children = new[]
+                                    {
+                                        new NTreeNode
+                                        {
+                                            Label = "J"
+                                        },
+                                        new NTreeNode
+                                        {
+                                            Label = "K"
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                        new NTreeNode
+                        {
+                            Label = "C",
+                            Children = new[]
+                            {
+                                new NTreeNode
+                                {
+                                    Label = "F",
+                                    Children = new[]
+                                    {
+                                        new NTreeNode
+                                        {
+                                            Label = "L"
+                                        },
+                                        new NTreeNode
+                                        {
+                                            Label = "M"
+                                        }
+                                    }
+                                },
+                                new NTreeNode
+                                {
+                                    Label = "G",
+                                    Children = new[]
+                                    {
+                                        new NTreeNode
+                                        {
+                                            Label = "N"
+                                        },
+                                        new NTreeNode
+                                        {
+                                            Label = "O"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+
+    internal class NTreeNode
+    {
+        public string Label { get; set; }
+
+        public IEnumerable<NTreeNode> Children { get; set; }
+    }
+}
---
+++ src/Anaximander.Linq/TreeTraversal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anaximander.Linq.TreeTraversal
+{
+    public static class TreeTraverser
+    {
+        // Adapted from code by MSDN user OlofPetterson
+        // https://social.msdn.microsoft.com/Forums/vstudio/en-US/63282240-0fbb-40c1-a716-1371e77a991d/getting-all-paths-in-tree-traversal#5819bb97-34cb-49ef-b202-031495157590-isAnswer
+        // retrieved 2019-09-23 12:32
+        public static IEnumerable<IEnumerable<TElement>> GetTraversalPaths<TElement>(TElement root, Func<TElement, IEnumerable<TElement>> childrenSelector)
+        {
+            Queue<PathBuilder<TElement>> pathBuilder = new Queue<PathBuilder<TElement>>();
+            pathBuilder.Enqueue(new PathBuilder<TElement>(root));
+
+            while (pathBuilder.Any())
+            {
+                PathBuilder<TElement> node = pathBuilder.Dequeue();
+                IEnumerable<TElement> children = childrenSelector(node.Current);
+
+                var isLeafNode = (children is null) || !children.Any();
+
+                if (isLeafNode)
+                {
+                    yield return node.Path;
+                }
+                else
+                {
+                    foreach (TElement child in children)
+                    {
+                        pathBuilder.Enqueue(node.Append(child));
+                    }
+                }
+            }
+        }
+
+        public class PathBuilder<TElement>
+        {
+            public PathBuilder(TElement root)
+            {
+                _path = new Queue<TElement>();
+                _path.Enqueue(root);
+            }
+
+            public PathBuilder(IEnumerable<TElement> path, TElement current)
+            {
+                _path = new Queue<TElement>(path);
+                _path.Enqueue(current);
+            }
+
+            private readonly Queue<TElement> _path;
+
+            public IEnumerable<TElement> Path { get => _path; }
+            public TElement Current => _path.Last();
+
+            public PathBuilder<TElement> Append(TElement next)
+            {
+                return new PathBuilder<TElement>(Path, next);
+            }
+        }
+    }
+}
